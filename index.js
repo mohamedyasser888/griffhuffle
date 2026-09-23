72b9c3eee71b6693ee05066ce66fdc84d0d1c4cc
mohamedyasser888 <moudyyasser02@gmail.com>
2026-09-23 18:51:30 +0300

    Analysis only - no changes made

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'admin' && password === 'password') {
+    res.send('Login successful');
+  } else {
+    res.status(401).send('Login failed');
+  }
+});
+
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
