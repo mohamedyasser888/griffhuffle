0b6c6d970017864efd57d71949c771bd94698d0d
mohamedyasser888 <moudyyasser02@gmail.com>
2026-09-23 18:48:43 +0300

    Analysis only - no changes made

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Default route - serves index.html
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.send('Hello World!');
 });
 
-// Serve CSS files from the public/css directory
-app.get('/css/:file', (req, res) => {
-  res.sendFile(__dirname + '/public/css/' + req.params.file);
-});
-
-// Serve JS files from the public/js directory
-app.get('/js/:file', (req, res) => {
-  res.sendFile(__dirname + '/public/js/' + req.params.file);
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
